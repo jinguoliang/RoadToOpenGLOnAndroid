66037c2ea10ebaaad35003dfde67f53b2215013c
jinux <jinguol999@163.com>
2017-02-06 08:01:35 +0800

    step-2-draw-one-point

--- app/src/main/java/com/jone/roadtoopenglonandroid/MainActivity.java
+++ app/src/main/java/com/jone/roadtoopenglonandroid/MainActivity.java
@@ -1,13 +1,8 @@
 package com.jone.roadtoopenglonandroid;
 
-import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,25 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GLSurfaceView surfaceView = (GLSurfaceView)findViewById(R.id.surfaceView);
-        surfaceView.setRenderer(new GLSurfaceView.Renderer() {
-            @Override
-            public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-                Log.e(TAG, "onSurfaceCreated");
-            }
-
-            @Override
-            public void onSurfaceChanged(GL10 gl, int width, int height) {
-                Log.e(TAG, "onSurfaceChanged");
-            }
-
-            @Override
-            public void onDrawFrame(GL10 gl) {
-                Log.e(TAG, "onDrawFrame");
-                GLES20.glClearColor(1f,1f,0f,1f);
-                GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-            }
-        });
+        GLSurfaceView surfaceView = (GLSurfaceView)findViewById(R.id.surfaceView1);
+        surfaceView.setEGLContextClientVersion(2);
+        surfaceView.setRenderer(new MRender());
         surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
     }
 }
